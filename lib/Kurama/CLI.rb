ccb3b93dd358f00dc62c68ebc610ca9f584eec06
s1l3ntq <qimiller89@gmail.com>
2020-12-02 18:08:31 -0600

    API flows thruough

--- lib/Kurama/CLI.rb
+++ lib/Kurama/CLI.rb
@@ -2,12 +2,17 @@ class CLI
     
     def start 
         puts "Welcome to the world of Breweries!"
-        puts "Select your favorite type of brew!"
+        puts "Press any key to continue."
+        # puts "Select your favorite brew company!"
         input = gets.chomp
         API.get_brewery_types(input)
+        list_results
+        
+    end
+    
+    def list_results
         Beer.all.each.each.with_index(1) do |b, index|
             puts "#{index}. #{b.name}"
         end
     end
-
 end
